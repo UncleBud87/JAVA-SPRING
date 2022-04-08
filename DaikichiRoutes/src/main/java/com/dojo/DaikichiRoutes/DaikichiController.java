ff63204126972ccf8650694765391e29e5c44cef
UncleBud87 <lperioux@gmail.com>
2022-04-08 13:12:55 -0500

    commit-1

--- DaikichiRoutes/src/main/java/com/dojo/DaikichiRoutes/DaikichiController.java
+++ DaikichiRoutes/src/main/java/com/dojo/DaikichiRoutes/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.dojo.DaikichiRoutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,4 +29,17 @@ public class DaikichiController {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
 	
+	@RequestMapping("/travel/{track}")
+	public String travel(@PathVariable("track") String track) {
+		return "Congratulations! you will soon travel to " + track;
+	}
+	
+	@RequestMapping("/lotto/{track}")
+	public String lotto(@PathVariable("track") Integer track) {
+		if(track % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		}else {
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
+	}
 }
