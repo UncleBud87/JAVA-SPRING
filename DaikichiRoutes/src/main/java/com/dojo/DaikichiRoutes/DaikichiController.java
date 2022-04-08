b97e6b600fcf4f901d4a4509b0675c3b15ffa0e8
UncleBud87 <lperioux@gmail.com>
2022-04-08 12:34:40 -0500

    commit-1

--- DaikichiRoutes/src/main/java/com/dojo/DaikichiRoutes/DaikichiController.java
+++ DaikichiRoutes/src/main/java/com/dojo/DaikichiRoutes/DaikichiController.java
@@ -2,6 +2,7 @@ package com.dojo.DaikichiRoutes;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,11 @@ public class DaikichiController {
 	@RequestMapping("")
 	public String index() {
 		return "Welcome!";
+	}
+	
+	@RequestMapping("/search")
+	public String index(@RequestParam(value="q", required=false) String searchQuery) {
+		return"You searched for: " + searchQuery;
 	}
 
 	@RequestMapping(value="/today", method=RequestMethod.GET)
@@ -21,4 +27,5 @@ public class DaikichiController {
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
+	
 }
